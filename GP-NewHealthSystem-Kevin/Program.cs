4c24fe6093d92374f3b92ed213afdb586ebaa159
KevX36 <kevincard2007@gmail.com>
2026-01-09 15:30:09 -0400

    added health class other than the max Health constructor

---
+++ GP-NewHealthSystem-Kevin/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GP_NewHealthSystem_Kevin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+
+    class health
+    {
+        public health(int HP)
+        {
+            _maxHealth = HP;
+            _currentHealth = HP;
+
+
+        }
+        
+
+
+
+
+        private int _maxHealth = 100;
+        private int _currentHealth = 100;
+        public void Restore()
+        {
+            _currentHealth = _maxHealth;
+        }
+        public void Heal(int healing)
+        {
+            _currentHealth += healing;
+            if (_currentHealth > _maxHealth)
+            {
+                _currentHealth = _maxHealth;
+            }
+        }
+
+        public int currentHP
+        {
+            get { return _currentHealth; }
+        }
+        public int maxHP
+        {
+            get { return _maxHealth; }
+        }
+
+
+
+
+
+
+
+    }
+
+    class player
+    {
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
